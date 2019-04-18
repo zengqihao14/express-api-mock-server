176edc4a9be53ddf457740ee33e1cf79ba286cee
zengqihao <zengqihao@videopls.com>
2019-04-18 17:33:20 +0800

    add apiHelper

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,23 @@
+const path = require('path');
+const apiHelper = require('../../middleware/apiHelper');
+
 module.exports = (app, config) => {
+  const API = new apiHelper(app);
   const URI = (path) => (config.API_BASE_PATH + path);
 
   // Example
-  app.post(URI('login'), (req, res) => {
+  API.POST(URI('login'), require('../data/auth/login'), (req, res) => {
     res.cookie('AuthToken', 'bad18eba1ff45jk7858b8ae88a77fa30', config.COOKIE_OPTIONS);
     res.cookie('uid', 12, config.COOKIE_OPTIONS);
-    res.send(require('../data/auth/login'));
   });
-  app.post(URI('logout'), (req, res) => {
+  API.POST(URI('logout'), require('../data/auth/logout'), (req, res) => {
     res.clearCookie('AuthToken');
     res.clearCookie('uid');
-    res.send(require('../data/auth/logout'));
   });
 
-  app.get(URI('users'), (req, res) => res.send(require('../data/user/getUsers')));
-  app.put(URI('user'), (req, res) => res.send(require('../data/user/newUser')));
-  app.get(URI('user/:id'), (req, res) => res.send(require('../data/user/getUser')));
-  app.post(URI('user/:id'), (req, res) => res.send(require('../data/user/updateUser')));
-  app.delete(URI('user/:id'), (req, res) => res.send(require('../data/user/deleteUser')));
+  API.GET(URI('users'), require('../data/user/getUsers'));
+  API.PUT(URI('user'), require('../data/user/newUser'));
+  API.GET(URI('user/:id'), require('../data/user/getUser'));
+  API.POST(URI('user/:id'), require('../data/user/updateUser'));
+  API.DELETE(URI('user/:id'), require('../data/user/deleteUser'));
 };
